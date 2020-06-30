eb46af0e442b0bb2f42a72e26beb69835c47b368
Adam <296786475@qq.com>
2020-06-30 21:21:46 +0800

    创建Adam_20200630_Memories项目.

--- Adam_20200630_Memories/ContentView.swift
+++ Adam_20200630_Memories/ContentView.swift
@@ -10,8 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: (index%2==0))
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+        
     }
+}
+
+struct CardView: View {
+    var isFaceUp : Bool
+    
+    var body: some View {
+        ZStack  {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10).fill()
+            }
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
