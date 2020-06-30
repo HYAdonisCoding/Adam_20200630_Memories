ba436fa6bda9e7fae9fb1419bbbd6a68bf948443
Adam <296786475@qq.com>
2020-06-30 14:35:37 +0800

    Initial Commit

---
+++ Adam_20200630_Memories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Adam_20200630_Memories
+//
+//  Created by Adam on 2020/6/30.
+//  Copyright © 2020 Adam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
